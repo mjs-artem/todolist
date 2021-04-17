ee469b5c5130a88ad756489eb3c8ad311a4a365c
mjs <mjs.artem@gmail.com>
2021-04-18 01:36:19 +0300

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>My first awesome application from APP! )))</div>;
+
+export default App;
--- src/Test/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import cls from "./index.module.scss";
-import classanems from "classnames";
-
-const Test = () => (
-  <div className={`${cls.app} ${true ? [cls.test1] : "test2"}`}>My first awesome application from TEST! )))</div>
-);
-
-export default Test;
