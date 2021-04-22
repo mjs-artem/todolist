82508af2716dbfcd37bb27e064caf2355930f905
Artem <artem@Artems-MacBook-Pro.local>
2021-04-22 22:00:17 +0300

    Fixed app component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App/App";
-import Test from "./Test/Test";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Test />
   </React.StrictMode>,
   document.getElementById("root"),
 );
